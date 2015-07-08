3efcbf1c39d2aa88f79ffa0d8777f0c11b37e3e4
bacheli <sercheli-25@hotmail.com>
2015-07-08 13:14:59 +0200

    ex enamoradita

---
+++ index.php
@@ -0,0 +1,135 @@
+
+<html lang="es">
+<head>
+  <meta content="Leydi" name="author"/>
+  <meta charset="utf-8">
+
+  
+  <title>Leydi</title>
+
+  <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="libs/css/animate.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+</head>
+<body >
+<header>
+  <nav class="navbar navbar-default navbar-fixed-top " role="navigation">
+  <div class="container">
+    
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+      <a class="navbar-brand animated rotateInDownLeft" href="index.html">LEYDI</a><br>
+
+    </div>
+  
+   
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+
+      <ul class="nav navbar-nav navbar-right animated lightSpeedIn">
+        <li><a href="paginas/datos.html">DATOS PERSONALESS</a></li>
+        <li><a href="paginas/estudios.html">ESTUDIOS REALIZADOS</a></li>
+        <li><a href="paginas/informacion.html">INFORMACION FAMILIAR</a></li>
+        <li><a href="paginas/curriculum.html">CURRICULUM VITAE</a></li>
+        <li><a href="paginas/areas.html">AREAS DE INTERES</a></li>
+
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">MAS<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+          <li><a href="paginas/paginas.html">PAGINAS IMPORTANTES</a></li>
+            <li><a href="paginas/formulario.html">FORMULARIO</a></li>
+            <li><a href="paginas/fotos.html">FOTOS</a></li>
+            <li><a href="https://www.facebook.com/" target="_blank"><img alt="siguenos en facebook" height="32" src="http://2.bp.blogspot.com/-q_Tm1PpPfHo/UiXnJo5l-VI/AAAAAAAABzU/MKdrVYZjF0c/s1600/face.png" title="siguenos en facebook" width="32" /></a></li>
+       <li> <a href="https://twitter.com/" target="_blank"><img alt="siguenos en Twitter" height="32" src="http://3.bp.blogspot.com/-wlwaJJG-eOY/UiXnHS2jLsI/AAAAAAAAByQ/I2tLyZDLNL4/s1600/Twitter+NEW.png" title="siguenos en Twitter" width="32" /></a></li>
+
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+    </div>
+  </nav>
+</header><br>
+<section style="padding-top: 50px">
+
+  <div class=" container jumbotron animated zoomIn"><br>
+    <div class="container text-center">
+      <marquee behavior="scroll">
+          <h1> LEYDI LILI SUARES LLATAS</h1>
+      </marquee>
+    </div>
+  </div>
+  </div>
+</section>
+<section>
+<div class="container galeria">
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+ 
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  </ol>
+
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <div class="row animated zoomIn">
+        <div class="col-sm-6">
+         <img src="img/leydi.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/lady.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="row animated zoomIn">
+        <div class="col-sm-6">
+         <img src="img/2.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/preciosa.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="row animated zoomIn">
+        <div class="col-sm-6">
+         <img src="img/bebe.jpg" alt="Leidy" class="img-thumbnail hidden-xs">
+        </div>
+        <div class="col-sm-6">
+         <img src="img/bebe1.jpg" alt="Leidy" class="img-thumbnail">
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+</div>
+ </section>
+ <section>
+ 	
+<h1>
+	sadklfsdfklsdjksndñgvklsdnmgvkñsdm
+</h1>
+
+ </section>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+</html>
---
+++ paginas/regformulario.php
@@ -0,0 +1,4 @@
+<?php
+	echo "<b>Esta es la pagina que recibe los datos del formulario</b><br>";
+	echo "falta leer campos y mostralos";
+?>
